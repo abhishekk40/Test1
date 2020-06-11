0767ff6ea7672c5dd574787c6fb39fe71c5630e3
abhishekk40 <66176753+abhishekk40@users.noreply.github.com>
2020-06-11 12:32:46 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,13 @@
-print("Test1")
+a=6
+y=0
+for i in range(1,6): # Always keep the first loop for the Count of Number of lines
+    for j in range(1,y+1): # This loop is for Printing Space
+        print(" ",end=" ")
+    y=y+1 #This is for increasing the number of spaces everytime
+    for k in range(1,a): #This loop is for Printing "1"
+        print("1",end=" ")
+    a=a-1 #This is for decreasing the value of "a" everytime
+
+    print("\n")
+        
+        
